cc406de967120044e1b004202de39e8d91ff09bb
gems-c79729ba <wh222@ic.ac.uk>
2022-10-10 23:55:39 +0100

    add the further function

---
+++ scripts/my_panda_example.py
@@ -0,0 +1,6 @@
+from envtest import my_panda_example
+
+w = 5
+j =4
+
+print(my_panda_example(w,j))
--- scripts/solve_matrix_equation.py
+++ scripts/solve_matrix_equation.py
@@ -7,5 +7,4 @@ from sympy.matrices import Matrix, MatrixSymbol
 A = Matrix([[2, 1, 3], [4, 7, 1], [2, 6, 8]])
 b = Matrix(MatrixSymbol('b', 3, 1))
 x = my_mat_solve(A, b)
-
 print(x)
